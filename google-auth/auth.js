fbd903d23dab8d34e63c01d46c0526ac959659b0
Yoel Martin <yoelmartinfornieles@gmail.com>
2021-09-29 18:40:13 +0200

    YM - WIP: googleOauth2

---
+++ google-auth/auth.js
@@ -0,0 +1,25 @@
+const passport = require("passport")
+const User = require("../models/User.model")
+
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID:     process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "HTTP://localhost:5005/google/callback",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+passport.serializeUser(function (user, done) {
+	done(null,user);
+})
+
+passport.deserializeUser(function (user, done) {
+	done(null,user);
+})
